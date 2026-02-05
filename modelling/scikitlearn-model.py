de3c7334b008d371e88d1a43d45096a671af2702
Austin <jeonghyeon076@gmail.com>
2026-02-05 02:15:22 -0500

    More detailed data collection for estimation of game winners, team totals, and spreads. Model to be made later

---
+++ modelling/scikitlearn-model.py
@@ -0,0 +1,11 @@
+import numpy as numpy
+import matplotlib as plt
+import pandas as pd
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+
+class ProbDecomp:
+    def __init__(self):
+        
